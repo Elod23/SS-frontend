663835c86ed682cc787310a9cbb7bddd4f96509b
Elod23 <vargaelod23@gmail.com>
2022-06-20 23:28:38 +0300

    Add dotenv config setup and Root for router

---
+++ src/pages/Root.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SSLogo } from "../components";
+
+export default function Root() {
+    return (
+        <div className="container">
+            <h1>Root</h1>
+            <SSLogo/>
+        </div>
+    );
+}
