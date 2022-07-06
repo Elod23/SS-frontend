ba55e343f2634f6ad8663a46f47c4865d9acbb78
Elod23 <vargaelod23@gmail.com>
2022-07-06 21:16:25 +0300

    Finish vercel deps

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,5 @@
 export const encrissBaseUrl = process.env.REACT_APP_ENCRISS_BASE_URL || '';
 export const shipengineBaseUrl = process.env.REACT_APP_SHIPENGINE_BASE_URL || '';
 export const shipengineApiKey = process.env.REACT_APP_SHIPENGINE_API_KEY || '';
+export const proxyUrl = process.env.REACT_APP_PROXY_URL || '';
+export const rootUri = process.env.REACT_APP_ROOT_URI || '';
