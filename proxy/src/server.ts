5ce0fe6e351a280a5c166fe057c9503ab96c6031
Elod23 <vargaelod23@gmail.com>
2022-07-11 23:26:27 +0300

    Remove http server cuz of clash

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -3,6 +3,4 @@ import app from './index';
 const portConfig = process.env.PORT;
 console.log(`Https server running on port ${portConfig}`);
 app.httpsServer.listen(portConfig);
-console.log(`Http server running on port 8080`);
-app.httpServer.listen('8080');
 
