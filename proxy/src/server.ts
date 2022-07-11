aa182d4f8dc0a6f7fb7a088ff43883511c4d9101
Elod23 <vargaelod23@gmail.com>
2022-07-11 23:58:13 +0300

    RUn server lon http

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -2,5 +2,5 @@
 import app from './index';
 const portConfig = process.env.PORT;
 console.log(`Https server running on port ${portConfig}`);
-app.httpsServer.listen(portConfig);
+app.httpServer.listen(portConfig);
 
