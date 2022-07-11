b713ee44a99b45904c487172e42d5e3529028203
Elod23 <vargaelod23@gmail.com>
2022-07-11 23:23:00 +0300

    Add https support

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,24 +1,41 @@
 import express, { Express } from 'express';
 import routes from './routes';
 import cors from 'cors';
+import fs from 'fs';
+import https from 'https';
+import http, { Server } from 'http';
+
+var key = fs.readFileSync(__dirname + '/../../cert.key', 'utf-8');
+var cert = fs.readFileSync(__dirname + '/../../cert.crt', 'utf-8');
+var options = {
+  key: key,
+  cert: cert
+};
+
 class App {
-  public server: Express;
+  public app: Express;
+  public httpServer: Server;
+  public httpsServer: Server;
 
   constructor() {
-    this.server = express();
+    this.app = express();
+    
 
     this.middlewares();
     this.routes();
+    this.httpServer = http.createServer(this.app);
+    this.httpsServer = https.createServer(options, this.app);
   }
 
   middlewares() {
-    this.server.use(cors());
-    this.server.use(express.json());
+    this.app.use(cors());
+    this.app.use(express.json());
   }
 
   routes() {
-    this.server.use(routes);
+    this.app.use(routes);
   }
 }
 
-export default new App().server;
+
+export default new App();
--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -1,6 +1,8 @@
 
 import app from './index';
 const portConfig = process.env.PORT;
-console.log(`App running on port ${portConfig}`);
-app.listen(portConfig);
+console.log(`Https server running on port ${portConfig}`);
+app.httpsServer.listen(portConfig);
+console.log(`Http server running on port 8080`);
+app.httpServer.listen('8080');
 
